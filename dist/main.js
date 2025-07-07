b96d1249327f9548c67e958406fcf2a3cb28fece
Genzite <genziteid@gmail.com>
2025-07-07 19:50:50 +0700

    earth computer version 1, update navbar and homepage.progres next development service page

---
+++ dist/main.js
@@ -0,0 +1,36 @@
+function toggleHamburger() {
+    const btn = document.getElementById('hamburger');
+    const sideBar = document.getElementById('sidebar');
+
+    btn.classList.toggle('open');
+    sideBar.classList.toggle('translate-x-full');
+    sideBar.classList.toggle('translate-x-0');
+  }
+
+  // Tutup sidebar saat klik di luar
+  document.addEventListener('click', function (event) {
+    const btn = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
+
+    const isClickInsideSidebar = sidebar.contains(event.target);
+    const isClickOnHamburger = btn.contains(event.target);
+    const isSidebarOpen = sidebar.classList.contains('translate-x-0');
+
+    if (!isClickInsideSidebar && !isClickOnHamburger && isSidebarOpen) {
+      toggleHamburger();
+    }
+  });
+
+function initTypingEffect(selector) {
+new Typed(selector, {
+    strings: ["Komputer", "Laptop"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+}
+
+// Jalankan saat halaman sudah siap
+document.addEventListener('DOMContentLoaded', function () {
+initTypingEffect('#typed-output');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['"Exo 2"', 'sans-serif'],
+        paragraf: ['Lato', 'sans-serif']
+      },
+      colors: {
+        greenBase: '#27AE60',
+        secondGreen: '#145A32',      
+        gray: '#EBEBEB',
+        night: '#2C3E50',
+        white: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
